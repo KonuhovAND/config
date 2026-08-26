153b83a67967e2d6e3081041c5da2a02a3fa5074
Andrey Lorem <107177236+KonuhovAND@users.noreply.github.com>
2026-08-26 23:33:09 +0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -460,6 +460,11 @@ echo
 echo "Data partition:"
 echo "  $DATA_DEVICE -> $DATA_MOUNT"
 
+cat '~/.bashrc'  << 'EOF'
+alias snapshots-list='sudo snapper -c root list'
+alias makesnap='sudo snapper -c root create --description '
+alias deletesnap='sudo snapper -c root delete '
+EOF
 
 sudo dnf install snapper
 sudo snapper -c root create-config /
